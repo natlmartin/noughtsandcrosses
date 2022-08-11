b2cb5d69af325e1fb6fe923b0c66ae59ad1907f8
natlmartin <106486643+natlmartin@users.noreply.github.com>
2022-08-11 22:56:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,89 +1,85 @@
 const state = {
-    gameElement: document.querySelector('.game')
+    gameElement: document.querySelector('.game'),
+    symbols: ['o', 'x'],
     // cells: [null, null, null, null, null, null, null, null, null]
-    cells: Array(9).fill(null)
-    symbols: ['O', 'X']
+    cells: Array(9).fill(null),
     winningCombinations: [
-        [0, 1, 2], // top row
-        [3 ,4 ,5], // middle row
-        [6, 7, 8], // bottom row
-        [0, 3, 6], // left column
-        [1, 4, 7], // middle column
-        [2, 5, 8], // right column 
-        [0, 4, 8], // left diagonal
-        [2, 4, 6], //right diagonal
+        [0,1,2], // top row
+        [3,4,5], // middle row
+        [6,7,8], // bottom row
+        [0,3,6], // left column
+        [1,4,7], // middle column
+        [2,5,8], // right column
+        [0,4,8], // left diagonal
+        [2,4,6]  // right diagonal
     ],
     gameFinished: false
 }
 
 function drawBoard() {
-  const gameElement.innerHTML = ''
+    state.gameElement.innerHTML = ''
 
-  for (let i = 0; i < 9; i++) {
-    const cell = document.createElement('div')
-    cell.classList.add('cell')
+    for (let i = 0; i < state.cells.length; i++) {
+        const cell = document.createElement('div')
+        cell.classList.add('cell')
 
-    if (state.cells[i]) { // does the cell have an x or an o? if so, this code runs
+        if (state.cells[i]) { // does the cell have an x or an o? if so, this code runs
+            const cellSymbol = document.createElement('p') // <p class="symbol"></p>
+            cellSymbol.innerText = state.cells[i]
+            cellSymbol.classList.add('symbol')
+            cell.append(cellSymbol)
+        } else { //otherwise it must be empty, so run this next section
+            cell.addEventListener('click', function () {
+                if (state.gameFinished) {
+                    return
+                }
 
-        const cellSymbol = document.createElement('p') // <p class="symbol"></p>
-        cellSymbol.innerText = state.cells[i]
-        cellSymbol.classList.add('symbol')
+                state.symbols.reverse()
+                const currentPlayerSymbol = state.symbols[0]
+    
+                state.cells[i] = currentPlayerSymbol
+    
+                drawBoard()
 
-        cell.append(cellSymbol)
-    } else { //otherwise it must be empty, so run this next section
+                if (checkForWinner()) {
+                    drawMessage(`${state.symbols[0]} won!`)
+                    state.gameFinished = true
+                    return
+                }
 
+                if (checkForDraw()) {
+                    drawMessage("It's a Draw!") // use double quotation if string contains speech mark
+                    state.gameFinished = true
+                }
+            })
+        }
+
+        state.gameElement.append(cell)
     }
+}
 
-    cell.addEventListener('click', function()) {
-        if (state.gameFinished) {
-            return
-        }
+function checkForWinner() {
+    return state.winningCombinations.some(combo => {
+        const cells = combo.map(index => state.cells[index]
+                                // the array does not have a null AND all of the values are the same
 
-        state.symbols.reverse()
-        state.cells[i] = state.symbols[O]
-
-        drawBoard()
-
-        if (checkForWinner('Congratulations! You Won')) {
-            state.gameFinished = true
-            drawMessage()
-        }
-
-        if (checkForDraw("It's a draw!")) { // use double quotation if string contains speech mark
-            state.gameFinished = true
-            drawMessage()
-        }
-    })
-
-    state.gameElement.append(cell)
-  }
-
-function checkForDraw() {
-    return state.cells.every(function (cell) {
-        return cell !== null
+        return !(cells.includes(null)) && new Set(cells).size === 1 // Set gives back an array but just with unique values
     })
 }
 
-  function drawMessage (message) {
+function checkForDraw() {
+    return state.cells.every(cell => cell !== null)
+}
+
+function drawMessage(message) {
     const banner = document.createElement('div')
     banner.classList.add('banner')
 
     const h1 = document.createElement('h1')
-    h1.innerText = message
-
+    h1.innerHTML = message
     banner.append(h1)
-    
-    state.gameElement.append(banner)
-}
 
-  function checkForWinner() {
-    return state.winningCombinations.some(function (combo) {
-        const cells = combo.map(function (index) {
-            return state.cells[index]
-        })
-        // the array does not have a null AND all of the values are the same
-    return !(cells.includes(null)) && new Set(cells).size === 1 // Set gives back an array but just with unique values
-    })
+    state.gameElement.append(banner)
 }
 
 drawBoard()
